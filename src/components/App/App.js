92fd675f0851e08c4a96c16a2cd1f6272a1e34db
Melanie Daoheuang <daoh0031@gmail.com>
2021-07-09 11:55:03 -0600

    Add app shell

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import AddFriend from '..AddFriend/AddFriend';
+import Friends from '..Friends/Friends';
+import Header from '../Header/Header';
+import UpcomingBirthdays from '..UpcomingBirthdays/UpcomingBirthdays';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello World
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <Header />
+      <UpcomingBirthdays />
+      <AddFriend />
+      <Friends />
+    </main>
+  )
 }
 
 export default App;
