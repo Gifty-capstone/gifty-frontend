d97f81a973bfefc16d001daa7c045096faab62e6
Melanie Daoheuang <daoh0031@gmail.com>
2021-07-10 14:21:26 -0600

    Add hook into addFriend component

--- src/components/AddFriend/AddFriend.js
+++ src/components/AddFriend/AddFriend.js
@@ -1,11 +1,18 @@
-// import react from 'react'
+import React, {useState} from 'react'
 import './AddFriend.css'
-import '../Form/Form.js'
+import Form from '../Form/Form.js'
 
 const AddFriend = () => {
+  const [showModal, setShowModal] = useState(false)
+
+  const openModal = () => {
+    //toggling it back and forth
+    setShowModal(prev => !prev)
+  }
   return (
     <section className='add-friend'>
-      <button className='add-friend-button'>Add Friend</button>
+      <button className='add-friend-button' onClick={openModal}>Add Friend</button>
+      <Form showModal={showModal} setShowModal={setShowModal} />
     </section>
   )
 }
