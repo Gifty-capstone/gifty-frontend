b09ff75a00eb521327c0ccf70b289895bb870950
kendallha <kendallhaw@gmail.com>
2021-07-10 12:06:36 -0700

    Add avatar object with integer keys and import images to Friends component

--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -1,5 +1,16 @@
-import './Friends.css'
-import React from 'react'
+import './Friends.css';
+import { useSelector, useDispatch } from 'react-redux';
+import bear from '../../assets/bear.png';
+import cat from '../../assets/cat.png';
+import dog from '../../assets/dog.png';
+import fox from '../../assets/fox.png';
+import giraffe from '../../assets/giraffe.png';
+import lion from '../../assets/lion.png';
+import octopus from '../../assets/octopus.png';
+import panda from '../../assets/panda.png';
+import rabbit from '../../assets/rabbit.png';
+import rhinoceros from '../../assets/rhinoceros.png';
+
 
 const Friends = () => {
   const dispatch = useDispatch()
@@ -8,13 +19,16 @@ const Friends = () => {
   const friendList = friends.map(friend => {
     return (
       <section className='friend' key={friend.id}>
-        
+        <img src='../../assets/bear.png'></img>
       </section>
 
     )
   })
   return (
-    <p>test</p>
+    <>
+    <p>IMAGE TEST</p>
+    <img src={bear}/>
+    </>
   )
 }
 
