298800eaccab4837705e8b6e2c5d12be496fd846
kendallha <kendallhaw@gmail.com>
2021-07-10 14:35:11 -0700

    Add state to track button click for form display

--- src/components/FriendPage/FriendPage.js
+++ src/components/FriendPage/FriendPage.js
@@ -6,23 +6,33 @@ import { useEffect, useState } from 'react';
 const FriendPage = ({ id }) => {
   const friends = useSelector(state => state.friends)
   const [friend, setFriend] = useState({})
+  const [showForm, setForm] = useState(false)
 
   useEffect(() => {
-    console.log(friends)
     setFriend(friends.find(friend => parseInt(friend.id) === id))
-  }, [friends])
+  }, [friends, id])
+
+  const displayForm = () => {
+    return (
+      <form>
+        <input type='text' placeholder='Gift idea'/>
+        <button>Save idea</button>
+      </form>
+    )
+  }
 
   return (
-    <main className='friendGiftPage'>
+    <main className='friend-gift-page'>
       <header>
         <img src={getIcon(id)}></img>
         <h1>{friend.name}</h1>
         <h2>{friend.birthday}</h2>
         <p>{friend.memo}</p>
       </header>
-      <section className='addAGift'>
-        <i className='fas fa-plus fa-3x'></i>
+      <section className='add-a-gift'>
+        <i className='fas fa-plus fa-3x' onClick={() => setForm(true)}></i>
         <h3>Add a gift idea</h3>
+        {showForm && displayForm()}
       </section>
 
     </main>
