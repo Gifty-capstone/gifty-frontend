64428631902ba201f1ef0ec49e713468fce32fa9
Melanie Daoheuang <daoh0031@gmail.com>
2021-07-10 15:00:58 -0600

    Create form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,9 +1,16 @@
 import './Form.css'
 
-const Form = () => {
+const Form = ({ showModal, setShowModal }) => {
   return (
-    <section className='form-container'>
-      <div>Modal</div>
+    <section className='form'>
+      {showModal ? 
+        <section className='form-container'>
+          <h1>Add New Friend</h1>
+          <input type='text' className='name' placeholder='Full Name'></input>
+          <input type='date' className='form-date'></input>
+          <button className='form-button'>Submit</button>
+        </section> 
+        : null}
     </section>
   )
 }
