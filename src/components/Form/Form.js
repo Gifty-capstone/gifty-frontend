8c42939e7adeb7e8ebf7e0961c28699cdc9e7c4e
Melanie Daoheuang <daoh0031@gmail.com>
2021-07-10 17:48:30 -0600

    Close form button

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -7,14 +7,14 @@ const Form = ({ showModal, setShowModal }) => {
       {showModal ? (
         <section className='form-background'>
           <section showModal={showModal} className='form-wrapper'>
-            <img src={require('../../assets/otters.jpeg')} alt='otters'></img>
+            <img src={require('../../assets/otters.jpeg')} alt='otters' className='form-img'></img>
             <div className='form-content'>
               <h1>Add New Friend</h1>
               <input type='text' className='name' placeholder='Full Name'></input>
               <input type='date' className='form-date'></input>
               <button className='form-button'>Submit</button>
             </div>
-            <MdClose aria-label='Close modal' onClick={() => setShowModal(prev => !prev)}/>
+            <MdClose aria-label='Close modal' className='close-form' onClick={() => setShowModal(prev => !prev)}/>
           </section>
         </section> 
       ) : null}
