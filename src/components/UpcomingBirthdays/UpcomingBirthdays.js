69e11a56ef577aeae294104fee57af963a37e193
Rachel Hendricks <rachelhen92@gmail.com>
2021-07-10 14:21:44 -0600

    Sort birthdays and display necessary info

--- src/components/UpcomingBirthdays/UpcomingBirthdays.js
+++ src/components/UpcomingBirthdays/UpcomingBirthdays.js
@@ -8,9 +8,27 @@ const UpcomingBirthdays = () => {
     const monthFromToday = new Date(today.getTime() + 30*24*60*60*1000);
     const currentDate = today.toISOString().split('T')[0];
     const currentMonthFromToday = monthFromToday.toISOString().split('T')[0];
-    return friend.birthday > currentDate && friend.birthday < currentMonthFromToday
+    return friend.birthday > currentDate && friend.birthday < currentMonthFromToday && friend.gift === 'pending '
+  });
+  const sortedBirthdays = upcomingBirthdays.sort((a, b) => new Date(a.birthday) - new Date(b.birthday))
+  const relevantBirthdays = sortedBirthdays.map(friend => {
+    return (
+      <article key={friend.id}>
+        <ul>
+          <li>{friend.name}</li>
+          <ul>
+            <li>{friend.birthday.split('2021-')[1]}</li>
+          </ul>
+        </ul>
+      </article>
+    )
   })
-  
+
+  return (
+    <section>
+      {relevantBirthdays}
+    </section>
+  )
 }
 
 export default UpcomingBirthdays;
