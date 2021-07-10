e48ee3ee9bfbdbd260432ffc65ed37347726f9f9
kendallha <kendallhaw@gmail.com>
2021-07-10 11:39:31 -0700

    Create outline of Friends component

--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -1,6 +1,18 @@
 import './Friends.css'
+import React from 'react'
 
 const Friends = () => {
+  const dispatch = useDispatch()
+  const friends = useSelector(state => state.friends)
+
+  const friendList = friends.map(friend => {
+    return (
+      <section className='friend' key={friend.id}>
+        
+      </section>
+
+    )
+  })
   return (
     <p>test</p>
   )
