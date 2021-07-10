591ca18265919e329058c00b56ba42c1f11c2f1b
Rachel Hendricks <rachelhen92@gmail.com>
2021-07-10 13:55:07 -0600

    Add variable to find upcomingBirthdays

--- src/components/UpcomingBirthdays/UpcomingBirthdays.js
+++ src/components/UpcomingBirthdays/UpcomingBirthdays.js
@@ -1,9 +1,16 @@
 import './UpcomingBirthdays.css';
+import { useSelector } from 'react-redux';
 
 const UpcomingBirthdays = () => {
-  return (
-    <p>test</p>
-  )
+  const friends = useSelector(state => state.friends);
+  const upcomingBirthdays = friends.filter(friend => {
+    const today = new Date();
+    const monthFromToday = new Date(today.getTime() + 30*24*60*60*1000);
+    const currentDate = today.toISOString().split('T')[0];
+    const currentMonthFromToday = monthFromToday.toISOString().split('T')[0];
+    return friend.birthday > currentDate && friend.birthday < currentMonthFromToday
+  })
+  
 }
 
 export default UpcomingBirthdays;
