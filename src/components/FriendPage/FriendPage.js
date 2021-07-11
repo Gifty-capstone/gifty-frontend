fa6b487a955b864e2faa9fc506dd50e8e573be9c
kendallha <kendallhaw@gmail.com>
2021-07-10 20:41:16 -0700

    Add conditional rendering for undefined friend and button to delete friend

--- src/components/FriendPage/FriendPage.js
+++ src/components/FriendPage/FriendPage.js
@@ -1,9 +1,11 @@
 import './FriendPage.css';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { getIcon } from '../../utilities/avatars';
 import { useEffect, useState } from 'react';
+import { removeFriend } from '../../actions';
 
 const FriendPage = ({ id }) => {
+  const dispatch = useDispatch()
   const friends = useSelector(state => state.friends)
   const [friend, setFriend] = useState({})
   const [showForm, setForm] = useState(false)
@@ -24,22 +26,31 @@ const FriendPage = ({ id }) => {
     )
   }
 
-  return (
-    <main className='friend-gift-page'>
-      <header>
-        <img src={getIcon(id)}></img>
-        <h1>{friend.name}</h1>
-        <h2>{friend.birthday}</h2>
-        <p>{friend.memo}</p>
-      </header>
-      <section className='add-a-gift'>
-        <i className='fas fa-plus fa-3x' onClick={() => setForm(true)}></i>
-        <h3>Add a gift idea</h3>
-        {showForm && displayForm()}
-      </section>
-
-    </main>
-  )
+  if (friend) {
+    return (
+      <main className='friend-gift-page'>
+        <header>
+          <img src={getIcon(id)}></img>
+          <h1>{friend.name}</h1>
+          <h2>{friend.birthday}</h2>
+          <p>{friend.memo}</p>
+          <button onClick={() => dispatch(removeFriend(friend))}>Delete {friend.name} from friends</button>
+        </header>
+        <section className='add-a-gift'>
+          <i className='fas fa-plus fa-3x' onClick={() => setForm(true)}></i>
+          <h3>Add a gift idea</h3>
+          {showForm && displayForm()}
+        </section>
+      </main>
+    )
+    } else {
+      return (
+        <main>
+          <h1>This friend could not be found. Please return to the main dashboard.</h1>
+          <button>Return to main</button>
+        </main>
+      )
+    }
 }
 
 export default FriendPage;
