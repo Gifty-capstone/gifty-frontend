69bd27065dd34481564bb6d3f8274cb3638a59fa
Melanie Daoheuang <daoh0031@gmail.com>
2021-07-12 16:32:58 -0600

    Set state with useRef

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,4 +1,5 @@
 import './Form.css';
+import { useRef } from 'react';
 import otters from '../../assets/otters.png';
 import { MdClose } from 'react-icons/md';
 import { useSelector, useDispatch } from 'react-redux';
@@ -11,21 +12,31 @@ const Form = ({ showModal, setShowModal }) => {
   // const [date, setDate] = useState('');
 
   const [friend, setFriend] = useState({})
+  const inputName = useRef();
+  const inputDate = useRef();
+  const inputMemo = useRef();
 
   const handleChange = (event) => {
     // setName(event.target['name'] = event.target.value);
     // setDate(event.target['date'] = event.target.value);
+
+    setFriend({
+      birthday: inputDate.current.value,
+      name: inputName.current.value,
+      memo: inputDate.current.value
+    })
   }
 
   //Hook for controlled forms
   // birthday, name, memo
-  // add memo to form && form labels
+
 
   const handleSubmit = () => {
     // dispatch(addFriend(name, date));
+    
+    console.log(inputName.current.value)
+    console.log(inputDate.current.value)
   }
-
-  // COMBINE NAME/DATE TO PASS AS ONE PARAM
 
   return (
     <>
@@ -36,11 +47,11 @@ const Form = ({ showModal, setShowModal }) => {
             <div className='form-content'>
               <h1>New Friend Form</h1>
               <label className='form-label'>Name:</label>
-              <input type='text' className='form-name' placeholder='Full Name' onChange={handleChange} name='name'></input>
+              <input type='text' className='form-name' placeholder='Full Name' onChange={handleChange} ref={inputName}></input>
               <label className='form-label'>Birthday:</label>
-              <input type='date' className='form-date' onChange={handleChange} name='date'></input>
+              <input type='date' className='form-date' onChange={handleChange} ref={inputDate}></input>
               <label className='form-label'>Notes:</label>
-              <input type='memo' className='form-memo' placeholder='Add notes here' name='memo'></input>
+              <input type='text' className='form-memo' ref={inputMemo}></input>
               <button className='form-button' onClick={handleSubmit}>Submit</button>
             </div>
             <MdClose aria-label='Close modal' className='close-form' onClick={() => setShowModal(prev => !prev)}/>
