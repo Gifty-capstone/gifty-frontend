a6b9d49ff0cbc2beb3001afdd5e3b0d273e89a79
Melanie Daoheuang <daoh0031@gmail.com>
2021-07-13 14:49:21 -0600

    Add mock data for gifts

---
+++ src/giftMockData.js
@@ -0,0 +1,18 @@
+import {gift1, gift2, gift3} from "./assets"
+
+export const giftMockData = {
+  "data": [
+    {
+      "name": "blah",
+      "image": gift1,
+    },
+    {
+      "name": "blah",
+      "image": gift2,
+    },
+    {
+      "name": "blah",
+      "image": gift3,
+    },
+  ]
+}
