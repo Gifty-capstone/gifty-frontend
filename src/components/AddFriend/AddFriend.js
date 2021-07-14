33ae320ce2649e50bfba4365e2edd6fa1f5113c7
kendallha <kendallhaw@gmail.com>
2021-07-13 17:56:28 -0700

    Style plus button icon to add a friend

--- src/components/AddFriend/AddFriend.js
+++ src/components/AddFriend/AddFriend.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react'
 import './AddFriend.css'
-import Form from '../Form/Form.js'
+import Form from '../Form/Form.js';
+// import { FaPlus } from "react-icons/fa";
+import Icon from '@material-ui/core/Icon';
 
 const AddFriend = () => {
   const [showModal, setShowModal] = useState(false);
@@ -11,8 +13,11 @@ const AddFriend = () => {
   }
   return (
     <section className='add-friend'>
-      <button className='add-friend-button' onClick={openModal}>Add Friend</button>
-      <Form showmodal={showModal} setShowModal={setShowModal} />
+      <article className='add-section'>
+        <Icon className='fa fa-plus' onClick={openModal} style={{ fontSize: 60 }}></Icon>
+        <label className='plus-label'>Add a friend</label>
+      </article>
+      <Form showModal={showModal} setShowModal={setShowModal} />
     </section>
   )
 }
