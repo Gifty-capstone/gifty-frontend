3fb31a698ddb3cfbfa8b9dbed67d8ea979ed8da6
kendallha <kendallhaw@gmail.com>
2021-07-13 21:00:13 -0700

    Add styling to friend list icons and names

--- src/components/AddFriend/AddFriend.js
+++ src/components/AddFriend/AddFriend.js
@@ -3,6 +3,7 @@ import './AddFriend.css'
 import Form from '../Form/Form.js';
 // import { FaPlus } from "react-icons/fa";
 import Icon from '@material-ui/core/Icon';
+import Typography from '@material-ui/core/Typography';
 
 const AddFriend = () => {
   const [showModal, setShowModal] = useState(false);
@@ -15,6 +16,7 @@ const AddFriend = () => {
     <section className='add-friend'>
       <article className='add-section'>
         <Icon className='fa fa-plus' onClick={openModal} style={{ fontSize: 60 }}></Icon>
+        <Typography variant="srOnly">Add a friend</Typography>
         <label className='plus-label'>Add a friend</label>
       </article>
       <Form showModal={showModal} setShowModal={setShowModal} />
