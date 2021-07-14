0a909adc795fb87f08a530c791710b9c6805f3ab
kendallha <kendallhaw@gmail.com>
2021-07-13 17:05:42 -0700

    Add gift image and styling for app name

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
-import './Header.css'
+import './Header.css';
+import gift from '../../assets/gift.png';
 
 const Header = ({ userName }) => {
   return (
     <header>
-      <h1 className='title'>Gifty</h1>
-      <h2 className='userName'>Welcome {userName}</h2>
+      <h1 className='title'><img src={gift}></img>Gifty</h1>
+      <h2 className='userName'>Welcome {user.name}</h2>
     </header>
   )
 }
