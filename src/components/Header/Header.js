017d430e3ebc35961aac2e8b929c087f0cdcdb71
kendallha <kendallhaw@gmail.com>
2021-07-13 17:21:09 -0700

    Style user name in header and layout of name and title

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,8 +4,8 @@ import gift from '../../assets/gift.png';
 const Header = ({ userName }) => {
   return (
     <header>
-      <h1 className='title'><img src={gift}></img>Gifty</h1>
-      <h2 className='userName'>Welcome {user.name}</h2>
+      <h1 className='title'><img className='gift' src={gift}></img>Gifty</h1>
+      <h2 className='userName'>{user.name}'s Gift Planner</h2>
     </header>
   )
 }
