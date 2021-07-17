bccfcd9bfdf121c21835d8573c2f70071128cb8a
Rachel Hendricks <rachelhen92@gmail.com>
2021-07-16 19:34:33 -0600

    Stub request and check header

--- cypress/integration/homepage_spec.js
+++ cypress/integration/homepage_spec.js
@@ -1,5 +1,106 @@
-describe('Feedback Loop login flows', () => {
-  it('Should confirm that true is equal to true', () => {
-    expect(true).to.equal(true)
-  });
-});
+describe('Home Page', () => {
+      beforeEach(() => {
+            cy.intercept('https://gifty-backend-rails.herokuapp.com/api/v1/users/1/friends', {
+                "data": {
+                  "id": "1",
+                  "type": "user",
+                  "attributes": {
+                    "name": "Dione Travis",
+                    "email": "user@email.com",
+                    "birthday": "2020-11-03"
+                  },
+                  "relationships": {
+                    "friends": {
+                      "data": [{
+                          "id": "1",
+                          "type": "friend"
+                        },
+                        {
+                          "id": "2",
+                          "type": "friend"
+                        },
+                        {
+                          "id": "3",
+                          "type": "friend"
+                        }]
+                      }
+                    }
+                  },
+                        "included": [{
+                          "id": "1",
+                          "type": "friend",
+                          "attributes": {
+                            "name": "Tyler",
+                            "birthday": "2020-12-11",
+                            "id": 1,
+                            "memo": " Enjoys things",
+                            "need_gift": false
+                          },
+                          "relationships": {
+                            "gifts": {
+                              "data": [{
+                                  "id": "1",
+                                  "type": "gift"
+                                },
+                                {
+                                  "id": "2",
+                                  "type": "gift"
+                                }
+                              ]
+                            }
+                          }
+                        }, {
+                          "id": "2",
+                          "type": "friend",
+                          "attributes": {
+                            "name": "Jack",
+                            "birthday": "2020-11-09",
+                            "id": 2,
+                            "memo": " Enjoys stuff",
+                            "need_gift": false
+                          },
+                          "relationships": {
+                            "gifts": {
+                              "data": [{
+                                  "id": "3",
+                                  "type": "gift"
+                                },
+                                {
+                                  "id": "4",
+                                  "type": "gift"
+                                }
+                              ]
+                            }
+                          }
+                        }, {
+                          "id": "3",
+                          "type": "friend",
+                          "attributes": {
+                            "name": "Sophie",
+                            "birthday": "2020-07-08",
+                            "id": 3,
+                            "memo": " Enjoys all sorts of things and stuff",
+                            "need_gift": true
+                          },
+                          "relationships": {
+                            "gifts": {
+                              "data": [{
+                                "id": "5",
+                                "type": "gift"
+                              }]
+                            }
+                          }
+                        }]
+
+                    })
+                  cy.visit('http://localhost:3000')
+                })
+
+              it('should show header', () => {
+                cy.get('h1').contains('Gifty')
+                  .get('img').eq(0)
+                  .should('have.attr', 'src', titleGift)
+                  .get('h2').contains('Dione Travis\'s Gift Planner')
+              });
+
+            });
