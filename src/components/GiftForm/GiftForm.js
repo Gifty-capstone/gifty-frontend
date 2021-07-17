2f8789cbc4ffff3a0894a0dc1aa380c33eafd8c1
kendallha <kendallhaw@gmail.com>
2021-07-16 21:23:09 -0700

    Create GiftForm component to create more modular structure

--- src/components/GiftForm/GiftForm.js
+++ src/components/GiftForm/GiftForm.js
@@ -27,13 +27,13 @@ const GiftForm = ({ userId, friendId, addNewGift }) => {
 
   return (
     <form>
-      <input 
-        onChange={(e) => handleChange(e)} 
-        value={giftIdea} 
-        type='text' 
+      <input
+        onChange={(e) => handleChange(e)}
+        value={giftIdea}
+        type='text'
         placeholder='Gift idea'/>
       <button onClick={(e) => handleSubmit(e)}>Save idea</button>
-      {error && 
+      {error &&
       <p>Your gift could not be added at this time. Please try again.</p>}
     </form>
   )
