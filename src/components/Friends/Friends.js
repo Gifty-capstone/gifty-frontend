e41c257d2d05cbe80f8c64cb79465225518e4ec7
Melanie Daoheuang <daoh0031@gmail.com>
2021-07-18 19:09:28 -0600

    Remove unused code and organize

--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 const Friends = () => {
   const friends = useSelector(state => state.friends);
   const friendList = friends.map(friend => {
+    
     return (
       <Link to={`/${friend.id}`} className='friend' key={friend.id}>
         <img src={getIcon(friend.id)} alt='friend avatar' className='avatar'></img>
--- src/components/UpcomingBirthdays/UpcomingBirthdays.js
+++ src/components/UpcomingBirthdays/UpcomingBirthdays.js
@@ -5,14 +5,18 @@ import DayJS from 'react-dayjs';
 
 const UpcomingBirthdays = () => {
   const friends = useSelector(state => state.friends);
+
   const upcomingBirthdays = friends.filter(friend => {
     const today = new Date();
     const monthFromToday = new Date(today.getTime() + 30*24*60*60*1000);
     const currentDate = today.toISOString().split('T')[0];
     const currentMonthFromToday = monthFromToday.toISOString().split('T')[0];
+    
     return friend.birthday > currentDate && friend.birthday < currentMonthFromToday
   });
-  const sortedBirthdays = upcomingBirthdays.sort((a, b) => new Date(a.birthday) - new Date(b.birthday))
+
+  const sortedBirthdays = upcomingBirthdays.sort((a, b) => new Date(a.birthday) - new Date(b.birthday));
+
   const relevantBirthdays = sortedBirthdays.map(friend => {
     return (
       <article key={friend.id} className='upcoming'>
@@ -34,7 +38,7 @@ const UpcomingBirthdays = () => {
   })
 
   return (
-    <section className='birthdayArea'>
+    <section className='birthday-area'>
       <h3 className='upcoming'>Upcoming Birthdays:</h3>
       {relevantBirthdays}
     </section>
