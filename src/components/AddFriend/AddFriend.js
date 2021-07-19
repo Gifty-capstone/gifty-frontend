9983e3cdfabcf5d268bab2af4d5619a4a4dce4ad
kendallha <kendallhaw@gmail.com>
2021-07-19 16:10:18 -0700

    Fix add friend button

--- src/components/AddFriend/AddFriend.js
+++ src/components/AddFriend/AddFriend.js
@@ -12,7 +12,7 @@ const AddFriend = ({ userId }) => {
   
   return (
     <section className='add-friend'>
-      <article onClick={openModal} className='add-section'>
+      <article className='add-section'>
         <FaPlus className='fa fa-plus' onClick={openModal}/>
         <label className='plus-label'>Add a friend</label>
       </article>
