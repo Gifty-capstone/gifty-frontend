5490e616d4350dabf087675a976ba5f500ef1bc8
Rachel Hendricks <rachelhen92@gmail.com>
2021-07-20 13:51:05 -0600

    Fix form issue

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -6,19 +6,16 @@ import { useState } from 'react';
 import { addFriend } from '../../actions';
 import { postFriend } from '../../utilities/apiCalls';
 import Confetti from 'react-confetti';
-
 const initialState = {
   name: '',
   birthday: '',
   memo: ''
 }
-
 const Form = ({ userId, showmodal, setShowModal }) => {
   const dispatch = useDispatch();
   const [friend, setFriend] = useState(initialState);
   const [error, setError] = useState(false);
   const [confetti, setConfetti] = useState(false);
-
   const handleChange = ({ target: {name, value} }) => {
     setFriend({
       ...friend,
@@ -26,11 +23,9 @@ const Form = ({ userId, showmodal, setShowModal }) => {
     })
     setConfetti(false);
   }
-
   const clearForm = () => {
     Array.from(document.querySelectorAll('input')).forEach(input => (input.value=''));
   };
-
   const createNewFriend = () => {
     postFriend(userId, friend)
       .then(data => {
@@ -40,96 +35,79 @@ const Form = ({ userId, showmodal, setShowModal }) => {
       })
       .catch(error => setError(true))
   };
-
   const handleSubmit = () => {
     createNewFriend();
     setError(false);
   };
-
   return (
     <>
       {showmodal ? (
         <section className='form-background'>
-<<<<<<< HEAD
-          <section className='form-wrapper'>
-=======
-          <section 
+          <section
           className='form-wrapper'>
             {confetti &&
               <Confetti
               width={560}
-              height={445} 
+              height={445}
               />
             }
->>>>>>> b9efaa98c8ba07e91976d47236894872a2140b93
             <img src={otters} alt='otters' className='form-img'></img>
             <div className='form-content'>
               <h1 className='form-title'>New Friend Form</h1>
               <label className='form-label'>Name:</label>
-<<<<<<< HEAD
-              <input type='text' className='form-name' placeholder='Full Name' onChange={handleChange} ref={inputName}></input>
-              <label className='birthday-label'>Birthday: (don't worry if you don't know the year, just pick the month and day)</label>
-              <input type='date' className='form-date' onChange={handleChange} ref={inputDate}></input>
-              <label className='form-label'>Notes:</label>
-              <input type='text' className='form-memo' onChange={handleChange} ref={inputMemo}></input>
-              {error &&
-              <p>Something went wrong, please try again.</p>}
-              <button className='form-button' onClick={handleSubmit}>Submit</button>
-=======
-              <input 
-                type='text' 
-                name='name' 
-                className='form-name' 
-                placeholder='Full Name' 
+              <input
+                type='text'
+                name='name'
+                className='form-name'
+                placeholder='Full Name'
                 onChange={handleChange}>
               </input>
-              <p 
-                className='error name-error' 
-                style={{visibility: error ? 
-                  "visible" 
+              <p
+                className='error name-error'
+                style={{visibility: error ?
+                  "visible"
                   : "hidden"}}>
                 *Name required
               </p>
-              <label 
+              <label
                 className='birthday-label'>
                 Birthday: (don't worry if you don't know the year, just pick the month and day)
               </label>
-              <input 
-                type='date' 
-                name='birthday' 
-                className='form-date' 
+              <input
+                type='date'
+                name='birthday'
+                className='form-date'
                 onChange={handleChange}>
               </input>
-              <p 
-                className='error birthday-error' 
-                style={{visibility: error ? 
-                  "visible" 
+              <p
+                className='error birthday-error'
+                style={{visibility: error ?
+                  "visible"
                   : "hidden"}}>
                 *Birthday required
               </p>
-              <label 
+              <label
                 className='form-label'>
                 Notes:
               </label>
-              <input 
-                type='text' 
-                name='memo' 
-                className='form-memo' 
+              <input
+                type='text'
+                name='memo'
+                className='form-memo'
                 onChange={handleChange}>
               </input>
-              <p 
-                className='error post-error' 
-                style={{visibility: error ? 
-                  "visible" 
+              <p
+                className='error post-error'
+                style={{visibility: error ?
+                  "visible"
                   : "hidden"}}>
                 Please enter in required input fields.
               </p>
-              <button 
-                className='form-button' 
+              <button
+                className='form-button'
                 onClick={handleSubmit}>
                 Submit
               </button>
->>>>>>> b9efaa98c8ba07e91976d47236894872a2140b93
             </div>
             <MdClose aria-label='Close modal' className='close-form'
             onClick={() => setShowModal(prev => !prev)
@@ -140,5 +118,4 @@ const Form = ({ userId, showmodal, setShowModal }) => {
     </>
   )
 }
-
 export default Form;
